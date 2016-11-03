12b7525f9fa58dad41d421f266bf886ce5bc621a
Jason Barnett <jason@thebarnetts.ws>
2016-11-03 14:51:04 -0500

    configures knex

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'dev'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'test'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+}
