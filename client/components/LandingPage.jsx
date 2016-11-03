94124410b953d8b8cd50dc4196b842bb99fd623f
Joey Steinberger <josamuel@umich.edu>
2016-11-03 16:29:04 -0500

    testing linter with semicolons

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from './Login'
-import Signup from './Signup'
-import Foundation from 'react-foundation'
+import Signup from './Signup';
+import Foundation from 'react-foundation';
 
 const LandingPage = (props) => {
   return (
