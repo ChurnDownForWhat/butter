73212a413b484be9ce624f9cbf0de15d995993a9
Jason Barnett <jason@thebarnetts.ws>
2016-11-08 14:48:26 -0600

    creates card ctrl methods... wishfully

--- server/controllers/card.js
+++ server/controllers/card.js
@@ -1,10 +1,29 @@
-var Card = require('../models/Card')
+const Card = require('../models/Card')
 
 module.exports = {
-  createCard: function(){},
-  getAllCards: function(){},
-  getOneCard: function(){},
-  updateCard: function(){},
-  removeCard: function(){},
-  getDefaults: function(){}
+  createCard: function(newCard){
+    Card.save(newCard)
+    .then(id => res.send(id))
+    .then(() => res.end())
+  },
+  getAllCards: function(userId){
+    Card.fetchAll(userId)
+    .then(cards => res.send(cards))
+    .then(() => res.end())
+  },
+  getOneCard: function(cardId){
+    Card.fetchOne(cardId)
+    .then(card => res.send(card))
+    .then(() => res.end())
+  },
+  updateCard: function(cardId, newInfo){
+    Card.update(cardId, newInfo)
+    .then(id => res.send(id))
+    .then(() => res.end())
+  },
+  removeCard: function(cardId){
+    Card.delete(cardId)
+    .then(name => res.send(name))
+    .then(() => res.end())
+  }
 }
