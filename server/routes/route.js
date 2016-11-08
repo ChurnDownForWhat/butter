6913569c4f46d556a6c468dfbf10e354e6b2d835
Jason Barnett <jason@thebarnetts.ws>
2016-11-08 12:22:57 -0600

    reroute cards

--- server/routes/route.js
+++ server/routes/route.js
@@ -12,6 +12,19 @@ router.route('/user/:id/card').post()
 
 router.route('/user/:id/card').get()
 
+//create card 
+router.route('api/cards').post()
+//get all cards
+router.route('api/users/:id/cards').get()
+//get one card
+router.route('api/cards/:id').get()
+//update card
+router.route('api/cards/:id').put()
+//delete card
+router.route('api/cards/:id').delete()
+//get default cards
+router.route('api/default').get()
+
 router.route('/auth/google').get(passport.authenticate('google', 
 	{ scope: 'https://www.googleapis.com/auth/plus.login' })
 )
