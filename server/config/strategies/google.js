c3fea18239cea90f33b775e1867a0354ad38866b
Darion Freeman <darionfman@gmail.com>
2016-11-08 16:10:48 -0600

    Creates a fully functional authentication routes and stores user data

--- server/config/strategies/google.js
+++ server/config/strategies/google.js
@@ -7,11 +7,36 @@ module.exports = function(){
   passport.use(new GoogleStrategy({
     clientID: process.env.CONKEY,
     clientSecret: process.env.CONSECRET,
-    callbackURL: 'http://localhost:4000/auth/google'
+    callbackURL: 'http://localhost:4000/auth/google/callback'
   },
   function(token, tokenSecret, profile, done) {
-    console.log(token)
-    return done(null, profile)
+    const email = profile.emails[0].value
+    const firstName = profile.name.givenName
+    const lastName = profile.name.familyName
+    User.fetch(email)
+    .then((user) => {
+      if(!user){
+        const newUser = {
+          email: email,
+          firstName: firstName,
+          lastName: lastName
+        }
+        User.save(newUser)
+        .then((id) => {
+          newUser.id = id
+          newUser.token = token
+          return done(null, newUser)
+        })
+        .catch((err) => {
+          throw new Error('Failed to save user info!')
+        })
+      } else {
+        return done(null, user)
+      }
+    })
+    .catch((err) => {
+      throw new Error('Fetch failed! Failed to contact database!')
+    })   
   }
 ))
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -5,30 +5,34 @@ const User = require('../controllers/user')
 const Card = require('../controllers/card')
 
 router.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../public/index.html'))
+  res.sendFile(Path.resolve(__dirname, '../Components/index.js'))
 })
 
-router.route('/api/user').post(user.createUser)
+router.route('/api/user').post(User.createUser)
 
 router.route('/api/user/:id').get()
 
 router.route('/api/user/:id').put()
 
 //create card 
-router.route('api/cards').post(Card.createCard)
+router.route('/api/cards').post(Card.createCard)
 //get all cards
-router.route('api/users/:id/cards').get(Card.getAllCards)
+router.route('/api/users/:id/cards').get(Card.getAllCards)
 //get one card
-router.route('api/cards/:id').get(Card.getOneCard)
+router.route('/api/cards/:id').get(Card.getOneCard)
 //update card
-router.route('api/cards/:id').put(Card.updateCard)
+router.route('/api/cards/:id').put(Card.updateCard)
 //delete card
-router.route('api/cards/:id').delete(Card.removeCard)
+router.route('/api/cards/:id').delete(Card.removeCard)
 //get default cards
 router.route('/api/default').get()
 
 router.route('/auth/google').get(passport.authenticate('google', 
-	{ scope: 'https://www.googleapis.com/auth/plus.login' })
+	{ scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/plus.login'
+    ]})
 )
 
 router.route('/auth/google/callback').get(passport.authenticate('google', 
