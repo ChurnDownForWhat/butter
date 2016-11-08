d5d5bf8f34b6e8996246ab6b44f2196f32ac2ab1
Jason Barnett <jason@thebarnetts.ws>
2016-11-08 14:35:22 -0600

    connects card endpoints to ctrl

--- migrations/20161103145755_Butter_dev.js
+++ migrations/20161103145755_Butter_dev.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('Users', function(table){
+    knex.schema.createTableIfNotExists('Users', function(table){
       table.increments('id').primary()
       table.string('firstName', 20).notNullable()
       table.string('lastName', 20).notNullable()
@@ -10,7 +10,7 @@ exports.up = function(knex, Promise) {
       table.integer('creditScore').nullable()
     }),
 
-    knex.schema.createTable('Cards', function(table){
+    knex.schema.createTableIfNotExists('Cards', function(table){
       table.increments('id').primary()
       table.string('name', 80)
       table.integer('user_id')
@@ -36,13 +36,13 @@ exports.up = function(knex, Promise) {
       table.integer('minSpend')
     }),
 
-    knex.schema.createTable('Categories', function(table){
+    knex.schema.createTableIfNotExists('Categories', function(table){
       table.increments('id')
       table.string('category', 30).notNullable()
       table.string('program', 30)
     }),
 
-    knex.schema.createTable('DefaultCards', function(table){
+    knex.schema.createTableIfNotExists('DefaultCards', function(table){
       table.increments('id').primary()
       table.string('name', 80)
       table.integer('cardType')
---
+++ server/controllers/card.js
@@ -0,0 +1,10 @@
+var Card = require('../models/Card')
+
+module.exports = {
+  createCard: function(){},
+  getAllCards: function(){},
+  getOneCard: function(){},
+  updateCard: function(){},
+  removeCard: function(){},
+  getDefaults: function(){}
+}
