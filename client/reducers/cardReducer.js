814f16ecf8c15062729717c44a9977b00d961981
Jason Barnett <jason@thebarnetts.ws>
2016-11-09 16:44:19 -0600

    lint fixes

--- client/reducers/cardReducer.js
+++ client/reducers/cardReducer.js
@@ -1,39 +1,39 @@
-var cardReducer = function (state = {}, action) {
+const cardReducer = function (state = {}, action) {
   console.log('cardReducer was called with state', state, 'and action', action)
 
   switch (action.type) {
-    case 'ADD_CARD':
-      return {
-        ...state,
-        card: action.value
-      }
-    case 'DELETE_CARD':
-      return {
-        ...state,
-        card: action.value
-      }
-    case 'UPDATE_CARD':
-      return {
-        ...state,
-        card: action.value
-      }
-    case 'VIEW_CARD':
-      return {
-        ...state,
-        card: action.value
-      }
-    case 'VIEW_ALL_CARDS':
-      return {
-        ...state,
-        user_id: action.value
-      }
-    case 'GET_DEFAULTS':
-      return {
-        ...state,
-        card: action.value
-      }
-    default:
-      return state;
+  case 'ADD_CARD':
+    return {
+      ...state,
+      card: action.value
+    }
+  case 'DELETE_CARD':
+    return {
+      ...state,
+      card: action.value
+    }
+  case 'UPDATE_CARD':
+    return {
+      ...state,
+      card: action.value
+    }
+  case 'VIEW_CARD':
+    return {
+      ...state,
+      card: action.value
+    }
+  case 'VIEW_ALL_CARDS':
+    return {
+      ...state,
+      user_id: action.value
+    }
+  case 'GET_DEFAULTS':
+    return {
+      ...state,
+      card: action.value
+    }
+  default:
+    return state
   }
 }
 
--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,14 +1,14 @@
-var userReducer = function (state = {}, action) {
+const userReducer = function (state = {}, action) {
   console.log('userReducer was called with state', state, 'and action', action)
 
   switch (action.type) {
-    case 'GET_USER':
-      return {
-        ...state,
-        user: action.value
-      }
-    default:
-      return state;
+  case 'GET_USER':
+    return {
+      ...state,
+      user: action.value
+    }
+  default:
+    return state
   }
 }
 
