6dfddc44b6460976983905c1c3e4b7bd68bf997e
Joey Steinberger <josamuel@umich.edu>
2016-11-08 19:28:41 -0600

    Default Cards are succesfully returned from the database and test are written

--- server/models/DefaultCard.js
+++ server/models/DefaultCard.js
@@ -2,11 +2,12 @@ const db = require('../config/db')
 
 const DefaultCard = {}
 
-DefaultCard.fetchAll = () => {
+DefaultCard.fetchAll = () => 
   db('DefaultCards').select('*')
   .then(rows => rows)
-  .catch(err => err)
-}
+  .catch(err => console.log('error is', err))
+
+
 
 
 
