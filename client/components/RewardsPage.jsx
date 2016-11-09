319d38fec7c8b7e583967b4336464715d9cc5ef5
Calvin Kniffin <calkniffin@gmail.com>
2016-11-09 16:29:55 -0600

    Everything but toggle menu working

--- client/components/RewardsPage.jsx
+++ client/components/RewardsPage.jsx
@@ -2,24 +2,32 @@ import React from 'react'
 import RewardView from './RewardView'
 require('../../Static Assets/static rewards/rewards.less')
 require('../../Static Assets/static rewards/rewards.css')
+require('../../node_modules/bootstrap/dist/css/bootstrap.min.css')
+
+const toggleBtn = document.querySelector('#menu-toggle')
+const menu = document.querySelector('#wrapper')
+// function toggleMenu () {
+//   menu.classList.toggle('toggled')
+// }
+
 
 const RewardsPage = (props) => {
   return (
     <div>
-    <div class="background">
-      <div class="row">
-        <div class="logo col-md-5">
-          <h3 class=''>REWARDS</h3>
+    <div className="background">
+      <div className="row">
+        <div className="logo col-md-5">
+          <h3 className=''>REWARDS</h3>
         </div>
-        <form class="form-inline">
-          <input class="col-lg-offset-2 col-md-5" placeholder="filter rewards"></input>
+        <form className="form-inline">
+          <input className="col-lg-offset-2 col-md-5" placeholder="filter rewards"></input>
         </form>
       </div>
-      <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-      <div class="toggled" id="wrapper">
+      <a href="#menu-toggle" className="btn btn-default" id="menu-toggle">Toggle Menu</a>
+      <div className="toggled" id="wrapper">
         <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
+            <ul className="sidebar-nav">
+                <li className="sidebar-brand">
                     <a href="#">Menu</a>
                 </li>
                 <li>
@@ -40,11 +48,11 @@ const RewardsPage = (props) => {
             </ul>
         </div>
         <div id="page-content-wrapper">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="tableContainer">
-                  <table class="table table-responsive table-hover">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="tableContainer">
+                  <table className="table table-responsive table-hover">
                     <thead>
                       <tr>
                         <th>#</th>
@@ -55,56 +63,56 @@ const RewardsPage = (props) => {
                       </tr>
                     </thead>
                     <tbody>
-                      <tr class="">
+                      <tr className="">
                         <th scope="row">1</th>
                         <td>Virgin Elevate</td>
                         <td>Airline</td>
                         <td>1935</td>
                         <td>link</td>
                       </tr>
-                      <tr class="er">
+                      <tr className="er">
                         <th scope="row">2</th>
                         <td>Hilton HHonors</td>
                         <td>Hotel</td>
                         <td>582</td>
                         <td>link</td>
                       </tr>
-                      <tr class="ess">
+                      <tr className="ess">
                         <th scope="row">3</th>
                         <td>SPG</td>
                         <td>General</td>
                         <td>15,487</td>
                         <td>link</td>
                       </tr>
-                        <tr class="">
+                        <tr className="">
                         <th scope="row">4</th>
                         <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
                       </tr>
-                        <tr class="">
+                        <tr className="">
                         <th scope="row">5</th>
                         <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
                       </tr>
-                        <tr class="">
+                        <tr className="">
                         <th scope="row">6</th>
                         <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
                       </tr>
-                        <tr class="">
+                        <tr className="">
                         <th scope="row">7</th>
                         <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
                       </tr>
-                        <tr class="">
+                        <tr className="">
                         <th scope="row">8</th>
                         <td></td>
                         <td></td>
@@ -115,8 +123,8 @@ const RewardsPage = (props) => {
                   </table>
                 </div>
               </div>
-              <div class="col-md-6 col-md-offset-1">
-                <div class="piechart"></div>
+              <div className="col-md-6 col-md-offset-1">
+                <div className="piechart"></div>
               </div>
             </div>
           </div>
