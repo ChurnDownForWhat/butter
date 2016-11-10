8d3966185722d7c2516b3aa23b8af7a51e727742
Calvin Kniffin <calkniffin@gmail.com>
2016-11-10 11:24:43 -0600

    Base outline of default cards JSON object

---
+++ dbDefaultCards.js
@@ -0,0 +1,262 @@
+[
+  {
+  'name': 'Chase Sapphire Preferred',
+  'cardType': 'Chase',
+  'annBenefit': '',
+  'annFeeAmt': 95,
+  'waivedFees': ,
+  'category_id': ,
+  'signupBonus': 50000,
+  'minSpend': 4000
+  },
+  {
+  'name': 'Mariott Rewards',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Citi DoubleCash',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Capital One Venture',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Citi Prestige',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Discover IT',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Chase Freedom',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Fidelity Visa',
+  'cardType': 'Visa',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'IHG',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Ink Cash',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Hyatt',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Southwest',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Blue Cash',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Sallie Mae',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Bank of America Travel Rewards',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Capital One Quicksilver Cash',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Discover it Cashback',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Wells Fargo Cashwise Visa',
+  'cardType': 'Visa',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Chase Freedom Unlimited',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'BankAmericard Cash Rewards',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Chase British Airways',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Barclays Arrival Plus',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Capital One Venture',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Barclays JetBlue Plus',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Citi Hilton Reserve',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  },
+  {
+  'name': 'Discover it Cashback Match',
+  'cardType': '',
+  'annBenefit': '',
+  'annFeeAmt': '',
+  'waivedFees': '',
+  'category_id': ,
+  'signupBonus': ,
+  'minSpend':
+  }
+]
