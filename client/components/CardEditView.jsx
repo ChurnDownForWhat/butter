7dff4942e1e734122c2328565ab333fc21da141d
Jason Barnett <jason@thebarnetts.ws>
2016-11-12 11:17:02 -0600

    html to jsx/react

--- client/components/CardEditView.jsx
+++ client/components/CardEditView.jsx
@@ -1,99 +1,100 @@
 import React from 'react'
 
-const cardEdit = (props) => {
+const cardEditView = ({ defaults }) => {
+  
   return (
-    <form class="col-sm-12">
-      <div class="row">
-        <img src="./CreditCard_325x132.png" class="img-responsive col-sm-4"/>
-        <ul class="col-sm-8">
-          <div class="topFormItem" >
-            <input type="text" class="creditInputForm col-md-4" placeholder="Card Name"></input>
-            <span class="col-md-3"> Card Name </span>
+    <form className="col-sm-12">
+      <div className="row">
+        <img src="" className="img-responsive col-sm-4"/>
+        <ul className="col-sm-8">
+          <div className="topFormItem" >
+            <input type="text" className="creditInputForm col-md-4" placeholder="Card Name"></input>
+            <span className="col-md-3"> Card Name </span>
           </div>
           <br/>
-            <div class="topFormItem">
-              <select name="Card Type" class="col-md-4">
+            <div className="topFormItem">
+              <select name="Card Type" className="col-md-4">
                 <option value="Visa">Visa</option> 
                 <option value="MasterCard" >MasterCard</option>
                 <option value="American Express">American Express</option>
                 <option value="Discover">Discover</option>
               </select>
-              <span class="col-md-3"> Card Type </span>
+              <span className="col-md-3"> Card Type </span>
             </div>
             <br/>
-            <div class="topFormItem">
-              <input type="text" class="creditInputForm col-md-4" placeholder="Last Four Digits"></input>
-              <span class="col-md-3"> Last Four Digits </span>
+            <div className="topFormItem">
+              <input type="text" className="creditInputForm col-md-4" placeholder="Last Four Digits"></input>
+              <span className="col-md-3"> Last Four Digits </span>
             </div>
           <br/></ul>
       </div>
-      <div class="row">
-        <ul class="col-sm-6">
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Spend This Month"></input>
-            <span class="col-md-3"> Spend This Month </span>
+      <div className="row">
+        <ul className="col-sm-6">
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Spend This Month"></input>
+            <span className="col-md-3"> Spend This Month </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Spend Total"></input>
-            <span class="col-md-3"> Spend Total </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Spend Total"></input>
+            <span className="col-md-3"> Spend Total </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Sign-up Bonus"></input>
-            <span class="col-md-3"> Sign-up Bonus </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Sign-up Bonus"></input>
+            <span className="col-md-3"> Sign-up Bonus </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Minimum Spend"></input>
-            <span class="col-md-3"> Minimum Spend </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Minimum Spend"></input>
+            <span className="col-md-3"> Minimum Spend </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input id="spendDate" class="creditInputForm col-md-4" placeholder="Spend Deadline"/>
-            <span class="col-md-3"> Spend Deadline </span>
+          <div className="bottomFormItem">
+            <input id="spendDate" className="creditInputForm col-md-4" placeholder="Spend Deadline"/>
+            <span className="col-md-3"> Spend Deadline </span>
           </div> 
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Bonus Currency"></input>
-            <span class="col-md-3"> Reward Type </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Bonus Currency"></input>
+            <span className="col-md-3"> Reward Type </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Bill Due Date"></input>
-            <span class="col-md-3"> Monthly Bill Due Date </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Bill Due Date"></input>
+            <span className="col-md-3"> Monthly Bill Due Date </span>
           </div>
           <br/>
         </ul>
-        <ul class="col-sm-6">
-          <div class="bottomFormItem">
-            <input id="appDate" class="creditInputForm col-md-4" placeholder="Application Date"/>
-            <span class="col-md-3"> Application Date </span>
+        <ul className="col-sm-6">
+          <div className="bottomFormItem">
+            <input id="appDate" className="creditInputForm col-md-4" placeholder="Application Date"/>
+            <span className="col-md-3"> Application Date </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Annual Benefit"></input>
-            <span class="col-md-3"> Annual Benefit </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Annual Benefit"></input>
+            <span className="col-md-3"> Annual Benefit </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
-            <input type="text" class="creditInputForm col-md-4" placeholder="Annual Fee"></input>
-            <span class="col-md-3"> Annual Fee </span>
+          <div className="bottomFormItem">
+            <input type="text" className="creditInputForm col-md-4" placeholder="Annual Fee"></input>
+            <span className="col-md-3"> Annual Fee </span>
           </div>
           <br/>
-          <div class="bottomFormItem">
+          <div className="bottomFormItem">
             <input type="checkbox"></input>
-            <span class="col-md-3"> Annual Fee Waived First Year? </span>
+            <span className="col-md-3"> Annual Fee Waived First Year? </span>
           </div>
           <br/>
-           <div class="bottomFormItem">
-            <input id="feeDate" class="creditInputForm col-md-4" placeholder="Annual Fee Date"/>
-            <span class="col-md-3"> Annual Fee Date </span>
+           <div className="bottomFormItem">
+            <input id="feeDate" className="creditInputForm col-md-4" placeholder="Annual Fee Date"/>
+            <span className="col-md-3"> Annual Fee Date </span>
           </div> 
           <br/>
-          <div class="bottomFormItem">
-            <input id="cancelDate" class="creditInputForm col-md-4" placeholder="Planned Cancellation Date"/>
-            <span class="col-md-3"> Planned Cancellation Date </span>
+          <div className="bottomFormItem">
+            <input id="cancelDate" className="creditInputForm col-md-4" placeholder="Planned Cancellation Date"/>
+            <span className="col-md-3"> Planned Cancellation Date </span>
           </div> 
           <br/>
         </ul>
@@ -103,4 +104,4 @@ const cardEdit = (props) => {
 }
 
 
-export default cardEdit 
+export default cardEditView
