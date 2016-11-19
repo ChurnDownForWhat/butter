71ea990ebad4acca3743f06578a85b88d44a75a4
Robalexg <alexg7070@yahoo.com>
2016-11-19 10:43:21 -0600

    Refactored and Improved React Router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
-import { Provider } from 'react-redux'
-import { getUser } from '../actions/actions'
-import CardsPage from './CardsPage'
-import RewardsPage from './RewardsPage'
-import Navbar from './Navbar'
-import CreditScore from './CreditScore'
-import Pie from './Pie'
-import Slice from './Slice'
 import Sidebar from './Sidebar'
-import Amazon from './Amazon'
+import * as Bs from 'react-bootstrap'
+
 
 export default class App extends React.Component {
+
   constructor (props) {
     super(props)
     this.state = {
@@ -19,18 +12,30 @@ export default class App extends React.Component {
     }
   }
 
+  onHover(){
+    this.setState({
+      hover:'menuDisplayed'
+    })
+  }
+
+  exitHover(){
+    this.setState({
+      hover:''
+    })
+  }
+
   render () {
     return (
-      <div>
-    <Provider store={ this.props.store } >
-        <Router history={hashHistory}>
-        <Route path="/" component={CardsPage}/>
-        <Route path="/cards" component={CardsPage} />
-        <Route path="/rewards" component={RewardsPage} />
-        <Route path='/amazon' component={Amazon}/>
-      </Router>
-    </Provider>
-    </div>
+      <div id='wrapper'>
+        <div id='sidebar-wrapper' onMouseOver={this.onHover.bind(this)} onMouseLeave={this.exitHover.bind(this)}>
+          <Sidebar display={this.state.hover}/>
+        </div>
+        <div id='page-content-wrapper' className={this.state.hover}>
+          <Bs.Grid fluid={true}>
+            { this.props.children }
+          </Bs.Grid>
+        </div>
+      </div>
     )
   }
 }
--- client/index.js
+++ client/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import { store } from './reducers/main'
+import { Provider } from 'react-redux'
 import App from './components/App'
+import CardsPage from './components/CardsPage'
+import RewardsPage from './components/RewardsPage'
+import Amazon from './components/Amazon'
 require('./css/sidebar.css')
 require('./css/creditCardsPage.css')
 require('./css/amazon.css')
 
 
 render(
-  <App store={ store }/>,
+  <Provider store={ store } >
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={CardsPage}/>
+          <Route path="/rewards" component={RewardsPage} />
+          <Route path='/amazon' component={Amazon}/>
+        </Route>
+      </Router>
+    </Provider>,
   document.getElementById('app')
 )
 
