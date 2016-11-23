2717421b5348bda94da9c3cc587bf6778a396586
Darion Freeman <darionfman@gmail.com>
2016-11-23 11:01:04 -0600

    Removed inferior object dot assign

--- client/components/NewCard_detailed.jsx
+++ client/components/NewCard_detailed.jsx
@@ -95,7 +95,7 @@ class DetailedNewCard extends React.Component {
   }
 
   getSuggestionValue(suggestion) {
-    let cardNew = Object.assign({}, this.state.cardSub)
+    let cardNew = {...this.state.cardSub}
     for(let key in suggestion) {cardNew[key] = suggestion[key]}
     this.setState({newCard: suggestion, cardSub: cardNew, cardType:suggestion.cardType, category:suggestion.category})
     return `${suggestion.name}`
