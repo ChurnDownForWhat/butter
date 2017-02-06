b5c499496cb137a834131dbc834801a53fd36723
Jason Barnett <jason@thebarnetts.ws>
2017-02-06 16:32:20 -0600

    DBURL changes

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,7 @@ module.exports = {
    // for elephant  connection: 'postgres://auphecqh:L62hxHecYm1VgGWuDs3QgBloecgSVdL7@elmer.db.elephantsql.com:5432/auphecqh',
  development: {
    client: 'pg',
-   connection: {
-     host: process.env.DOIP,
-     user: process.env.DBUSER,
-     password: process.env.DBPASS,
-     database: process.env.DBNAME
-   },
+   connection: process.env.DATABASE_URL,
    migrations: {
      tableName: 'knex_migrations'
    }
